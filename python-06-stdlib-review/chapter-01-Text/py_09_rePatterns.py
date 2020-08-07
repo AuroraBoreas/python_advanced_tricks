9f5d84654a2c0ede23c3ff99fd0a647c7144ae53
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2020-08-07 11:55:27 +0800

    Add files via upload

--- python-06-stdlib-review/chapter-01-Text/py_09_rePatterns.py
+++ python-06-stdlib-review/chapter-01-Text/py_09_rePatterns.py
@@ -25,5 +25,26 @@ def test_patterns(patterns, text):
         print()
     return
 
+@addBreaker
+def test_patterns_updated_ver(patterns, text):
+    """Given source text and a list of patterns, look for
+    matches for each pattern within the text and print them to stdout
+    """
+    # look for each pattern in the text and print the results
+    for pattern, desc in patterns:
+        print('{!r} ({})\n'.format(pattern, desc))
+        print('  {!r}'.format(text))
+        for match in re.finditer(pattern, text):
+            s = match.start()
+            e = match.end()
+            prefix = ' ' * (s)
+            print('  {}{!r}{} '.format(prefix, text[s:e], ' '*(len(text) - e)), end=' ')
+            print(match.groups())
+            if match.groupdict():
+                print('{}{}'.format(' '*(len(text) - s), match.groupdict()))
+        print()
+    return
+    
 if __name__ == "__main__":
     test_patterns(patterns=[('ab', "'a' followed by 'b'")], text=text)
+    test_patterns_updated_ver(patterns=[(r'a((a*)(b*))', "'a' followed by 0-n 'a' and 0-n 'b'")], text='abbaabbba')
